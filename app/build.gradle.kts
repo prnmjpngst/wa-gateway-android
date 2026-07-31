e836b39b026cf8eaec7e604a1f0fb73886a681dd
ajipgt <financiaji@gmail.com>
2026-07-31 01:19:26 +0000

    Fix sheets API dependency version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,7 +96,7 @@ dependencies {
 
     implementation("com.google.api-client:google-api-client:2.2.0")
     implementation("com.google.auth:google-auth-library-oauth2-http:1.19.0")
-    implementation("com.google.apis:google-api-services-sheets:v4-rev20230926-2.0.0")
+    implementation("com.google.apis:google-api-services-sheets:v4-rev20250603-2.0.0")
 
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.jakewharton.timber:timber:5.0.1")
